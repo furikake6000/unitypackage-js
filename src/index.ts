b600cad57a763d0591d2f9b3c9138b7d0d8cd7b0
furikake6000 <ewave5000@gmail.com>
2025-12-13 05:19:25 +0000

    remove: 仮テストの削除 #4

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,3 @@ export {
   type UnityAsset,
   type UnityPackageInfo,
 } from './unitypackage';
-
-// TODO: 別のテストが追加されたら削除する
-export const hello = () => {
-  return 'Hello from unitypackage-js';
-};
